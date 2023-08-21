d575fc506c9fd2bae174c6e7d356331112013c4c
Raikyou <zoeymojin@gmail.com>
2023-08-21 14:09:04 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -306,7 +306,7 @@ Type=simple
 User=root
 Group=root
 WorkingDirectory=/root
-ExecStart=/usr/bin/python3 /root/client-linux.py SERVER=raikyou.online USER=%H PASSWORD=wasd
+ExecStart=/usr/bin/python3 /root/client-linux.py SERVER=freeuse.eu.org USER=%H PASSWORD=wasd
 Restart=always
 #Environment=DEBUG=true
 
